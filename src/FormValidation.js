edc5e19e854d37a1f925fd1e30808c057b3cbd4f
jdegaris <j.a.degaris@gmail.com>
2019-05-29 23:03:25 -0400

    try again

---
+++ src/FormValidation.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+
+function FormValidation(initialState, validate) {
+    const [values, setValues] = useState(initialState);
+    const [errors, setErrors] = useState({});
+    const [isSubmitting, setSubmitting] = useState(false);
+
+    useEffect(() => {
+        if (isSubmitting) {
+            const noErrors = Object.keys(errors).length === 0 
+            if (noErrors) {
+                console.log('authenticated', values.email, values.password);
+                setSubmitting(false)
+            } else {
+                setSubmitting(false)
+            }
+        }
+
+    }, [errors])
+
+    function handleChange(e) {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    function handleBlur() {
+        const validationErrors = validate(values);
+        setErrors(validationErrors)
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        const validationErrors = validate(values);
+        setErrors(validationErrors)
+        setSubmitting(true);
+        
+    }
+
+
+    return { handleChange, handleSubmit, handleBlur, values, errors, isSubmitting }
+}
+
+export default FormValidation
--- src/index.js
+++ src/index.js
@@ -1,12 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import FormValidation from './FormValidation';
+import validateAuth from './ValidateAuth';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const INITIAL_STATE = {
+    email: '',
+    password: ''
+}
+
+function Register() {
+    // execute FormValidation
+    const { 
+        handleChange, 
+        handleSubmit, 
+        handleBlur, 
+        values, 
+        errors, 
+        isSubmitting
+    } = FormValidation(INITIAL_STATE, validateAuth);
+
+    
+    return (
+        <div className="container">
+            <h1>Form Title</h1>
+            <form onSubmit={ handleSubmit }>
+                <input 
+                    className={errors.email && 'error-input'}
+                    onChange={ handleChange }
+                    onBlur={ handleBlur }
+                    name="email"
+                    type="email"
+                    value={values.email}
+                    autoComplete="off"
+                    placeholder="Your email address"
+                />
+                {errors.email  && <p className="error-text">{errors.email}</p>}
+                <input 
+                    className={errors.password && 'error-input'}
+                    onChange={ handleChange }
+                    onBlur={ handleBlur }
+                    name="password"
+                    type="password"
+                    value={values.password}
+                    placeholder="Choose a safe password"
+                  />
+                  {<errors className="password"></errors>  && <p className="error-text">{errors.password}</p>}
+            <div className='submit'>
+                <button disabled={isSubmitting} type="submit">Submit</button>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<Register />, rootElement);
